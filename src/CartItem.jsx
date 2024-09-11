da8ca2d2932c2df1f52e304ffaa248bcba7a2a90
Mayank <agarwalmayank15@gmail.com>
2024-09-11 15:36:30 -0400

    Cart Change

--- src/CartItem.jsx
+++ src/CartItem.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { useSelector, useDispatch } from 'react-redux';
-import { removeItem, updateQuantity } from './CartSlice';
+import { addItem, removeItem, updateQuantity } from './CartSlice';
 import './CartItem.css';
 
 const CartItem = ({ onContinueShopping }) => {
@@ -23,11 +23,13 @@ const CartItem = ({ onContinueShopping }) => {
 const handleCheckoutShopping = (e) => {
   alert('Functionality to be added for future reference');
 };
+const handleInc = (product) => {
+    dispatch(addItem(product));
+     };
 
-  const handleIncrement = (item) => {
+ const handleIncrement = (item) => {
     dispatch(addItem(item));
   };
-
   const handleDecrement = (item) => {
     if(item.quantity===1){
       dispatch(removeItem(item.name));
@@ -58,7 +60,7 @@ const handleCheckoutShopping = (e) => {
               <div className="cart-item-quantity">
                 <button className="cart-item-button cart-item-button-dec" onClick={() => handleDecrement(item)}>-</button>
                 <span className="cart-item-quantity-value">{item.quantity}</span>
-                <button className="cart-item-button cart-item-button-inc" onClick={() => handleIncrement(item)}>+</button>
+                <button className="cart-item-button cart-item-button-inc" onClick={() => handleInc(item)}>+</button>
               </div>
               <div className="cart-item-total">Total: ${calculateTotalCost(item)}</div>
               <button className="cart-item-delete" onClick={() => handleRemove(item)}>Delete</button>
