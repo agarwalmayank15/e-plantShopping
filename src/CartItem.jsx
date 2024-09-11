b9dddb5b3b4da057196e6703a1fc980e4b959657
Mayank <agarwalmayank15@gmail.com>
2024-09-11 14:25:24 -0400

    Cart Change

--- src/CartItem.jsx
+++ src/CartItem.jsx
@@ -9,27 +9,40 @@ const CartItem = ({ onContinueShopping }) => {
 
   // Calculate total amount for all products in the cart
   const calculateTotalAmount = () => {
- 
+    let totalAmount=0;
+    cart.map((plant)=>{
+      totalAmount+=parseInt(plant.cost.slice(1))*plant.quantity;
+    })                                             
+    return totalAmount;
   };
 
   const handleContinueShopping = (e) => {
-   
+    onContinueShopping(e);
   };
 
-
+const handleCheckoutShopping = (e) => {
+  alert('Functionality to be added for future reference');
+};
 
   const handleIncrement = (item) => {
+    dispatch(addItem(item));
   };
 
   const handleDecrement = (item) => {
-   
+    if(item.quantity===1){
+      dispatch(removeItem(item.name));
+    } else{
+      dispatch(updateQuantity({name:item.name,quantity:item.quantity-1}));
+    }
   };
 
   const handleRemove = (item) => {
+    dispatch(removeItem(item.name));
   };
 
   // Calculate total cost based on quantity for an item
   const calculateTotalCost = (item) => {
+    return item.quantity*parseInt(item.cost.slice(1));
   };
 
   return (
